cf4d17e06b655c087166fdb3b0ea57e4f832421e
Usagi Ito <usagi@WonderRabbitProject.net>
2012-12-05 07:21:30 +0900

    ・ｗ・ｗ・ｗ・

--- native-client_wrp_project_template_kit/include/wrp/log.hxx
+++ native-client_wrp_project_template_kit/include/wrp/log.hxx
@@ -13,19 +13,26 @@ namespace WonderRabbitProject {
 
   class log{
   private:
+#ifndef WRP_NO_LOG
     const decltype(std::chrono::system_clock::now()) start;
     const std::string object_name;
     const void* object_address;
     std::string indent;
     static int nesting_counter;
+#endif
   public:
+#ifndef WRP_NO_LOG
     typedef std::chrono::duration<double> unit;
+#endif
     std::stringstream buffer;
     explicit log(const std::string object_name_ = "", const void* object_address_ = 0)
+#ifndef WRP_NO_LOG
       : start(std::chrono::system_clock::now())
       , object_name(object_name_)
       , object_address(object_address_)
+#endif
     {
+#ifndef WRP_NO_LOG
       ++nesting_counter;
       indent.resize(2 * nesting_counter);
       boost::fill(indent, ' ');
@@ -36,7 +43,9 @@ namespace WonderRabbitProject {
                   << std::chrono::duration_cast<unit>(start.time_since_epoch()).count()
         << std::endl
         ;
+#endif
     }
+#ifndef WRP_NO_LOG
     ~log(){
       auto end = std::chrono::system_clock::now();
       std::string b;
@@ -51,13 +60,19 @@ namespace WonderRabbitProject {
         ;
       --nesting_counter;
     }
-    auto operator<<(const bool v) -> decltype(buffer<<v) { return buffer << v; }
-    auto operator<<(const char* v) -> decltype(buffer<<v) { return buffer << v; }
-    auto operator<<(std::string& v) -> decltype(buffer<<v) { return buffer << v; }
-    auto operator<<(int& v) -> decltype(buffer<<v){ return buffer << v; }
+    auto operator<<(const bool v) -> decltype(buffer<<v) { return buffer; }
+    auto operator<<(const char* v) -> decltype(buffer<<v) { return buffer; }
+    auto operator<<(std::string& v) -> decltype(buffer<<v) { return buffer; }
+    auto operator<<(int& v) -> decltype(buffer<<v){ return buffer; }
+#else
+    auto operator<<(const bool v) -> decltype(buffer<<v) { return buffer; }
+    auto operator<<(const char* v) -> decltype(buffer<<v) { return buffer; }
+    auto operator<<(std::string& v) -> decltype(buffer<<v) { return buffer; }
+    auto operator<<(int& v) -> decltype(buffer<<v){ return buffer; }
+#endif
   };
-  
+#ifndef WRP_NO_LOG  
   int log::nesting_counter = -1;
-  
+#endif
 }
 
